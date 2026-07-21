fbd312a2114338308c6af1dc85fc66e677ee8bfc
JhonAlisHarol <aharolj@gmail.com>
2026-07-21 08:47:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def pantalla_login():
 
     # Base de datos local de usuarios
     usuarios_permitidos = {
-        "CONC5": "12345",
+        "JAVIER GONZALEZ": "12345678",
         "ALISJHON": "199432",
         "ISMAEL SAMUDIO": "20626", "DAMIAN NAVARRO": "DAMIAN26"
     }
